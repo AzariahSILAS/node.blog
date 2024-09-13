df9edb8c5f4c6d60451ad97cafd959b1f799d3fd
AzariahSILAS <95987530+AzariahSILAS@users.noreply.github.com>
2024-09-13 11:38:16 -0700

    the front end is almost done but i need to add a little more however, its not broken now so im saving it right now.

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,7 +3,7 @@ const { get } = require('mongoose');
 const router = express.Router();
 const Post = require('../models/Post')
 
-//  GET
+//  GET/
 //  HOME
 router.get('', async (req, res) => {
     try {
@@ -37,7 +37,52 @@ router.get('', async (req, res) => {
     
 });
 
+//  GET/
+//  Post :id
 
+router.get('/post/:id', async (req, res) => {
+    try {
+        let slug = req.params.id
+        const data = await Post.findById({_id: slug});
+        const locals = {
+            title: data.title,
+            description: "simple Blog created with NodeJs, Express & MongoDb."
+        }
+        res.render('post', {locals, data});
+    }   catch (error) {
+        console.log(error);
+    }
+})
+
+//  POST/
+//  POST - searchTerm
+
+router.post('/search', async (req, res) => {
+    try {
+        const locals = {
+            title: "Search",
+            description: "simple Blog created with NodeJs, Express & MongoDb."
+        }
+        let searchTerm = req.body.searchTerm;
+        const searchNoSpecialChar = searchTerm.replace(/[^a-zA-Z0-9 ]/g, "")
+
+      
+    const data = await Post.find({
+        $or: [
+            { title: { $regex: new RegExp(searchNoSpecialChar, 'i')}},
+            { body: { $regex: new RegExp(searchNoSpecialChar, 'i')}}
+        ]
+    });
+
+        
+        res.render("search", {
+            data,
+            locals
+        });
+    }   catch (error) {
+        console.log(error);
+    }
+})
 
 
 
