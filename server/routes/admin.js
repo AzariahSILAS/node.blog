b7b94bf943771fca83ce924e04e4decb0344bd81
AzariahSILAS <95987530+AzariahSILAS@users.noreply.github.com>
2024-09-17 07:24:46 -0700

    I just created A working register form thats saving an account and password to the dataBase

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post')
+const User = require('../models/User')
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const adminLayout = '../views/layouts/admin';
-//  POST/
+//  GET/
 //  Admin - Login Page
 
 router.get('/admin', async (req, res) => {
@@ -18,6 +21,70 @@ router.get('/admin', async (req, res) => {
         console.log(error);
     }
 });
+
+//  POST/
+//  Admin - Check Login
+
+
+router.post('/admin', async (req, res) => {
+    try {
+       
+        const {username, password} = req.body;
+
+        res.redirect('/admin');
+
+
+
+
+        // res.render('admin/index', { locals, layout: adminLayout });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+
+
+
+//  POST/
+//  Admin - Register
+
+
+router.post('/register', async (req, res) => {
+    try {
+        const {username, password} = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10)
+
+      
+        try {
+            const user = await User.create({ username, password:hashedPassword });
+            res.status(201).json({ message: 'User Created', user});
+        } catch (error) {
+            if(error.code === 11000) {
+                res.status(409).json({ message: 'User already active'});
+            }
+            res.status(500).json({ message: 'Internal server error'});
+            
+        }
+
+
+
+        // res.render('admin/index', { locals, layout: adminLayout });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 module.exports = router;
